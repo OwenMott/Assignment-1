0cc106b87df1b20c75e3e4193d7be7cce67a95f8
FSD38\owen.mott <owen.mott@gm.fsd38.ab.ca>
2020-09-14 08:41:02 -0600

    i did controls lol

---
+++ ASSIGNMENT 1/Assets/Scripts/playerManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerManager : MonoBehaviour {
+
+    Rigidbody2D rb2d;
+
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("its working");
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKey("w"))
+        {
+            rb2d.velocity = new Vector2(0, 2);
+        }
+        else if (Input.GetKey("s"))
+        {
+            rb2d.velocity = new Vector2(0, -2);
+        }
+        else if (Input.GetKey("d"))
+        {
+            rb2d.velocity = new Vector2(2, 0);
+        }
+        else if (Input.GetKey("a"))
+        {
+            rb2d.velocity = new Vector2(-2, 0);
+        }
+
+    }
+
+}
