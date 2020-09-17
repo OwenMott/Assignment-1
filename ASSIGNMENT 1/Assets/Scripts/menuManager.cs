7ae21d640ea393f70ec54003dbd5ca85f551f494
FSD38\owen.mott <owen.mott@gm.fsd38.ab.ca>
2020-09-17 11:12:03 -0600

    this is the entire weeks work, did a whole lot on the title page

---
+++ ASSIGNMENT 1/Assets/Scripts/menuManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuManager : MonoBehaviour {
+
+
+    public GameObject player;
+    public GameObject menu;
+    public GameObject pointer;
+    public GameObject playerNumber;
+    public GameObject[] pointerPostions;
+    Text actualNum;
+
+   
+
+    bool onButton;
+
+    // Use this for initialization
+    void Start () {
+        onButton = false;
+        actualNum = playerNumber.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // if button press down, move pointer
+        // move character
+        
+        // if button press up, move pointer
+        // flip the boolean onButton
+       if (Input.GetKeyDown("s") || Input.GetKeyDown("down") || Input.GetKeyDown("w") || Input.GetKeyDown("up"))
+       {
+
+            if (onButton == false)
+            {
+                onButton = true;
+            }  else 
+            {
+                onButton = false;
+            }
+            
+        }
+
+
+        if (onButton == false)
+        {
+            pointer.transform.position = pointerPostions[0].transform.position;
+        }
+        else
+        {
+            pointer.transform.position = pointerPostions[1].transform.position;
+        }
+
+
+        //if return is pressed and onButton equals true
+        //then start the game
+
+        if (Input.GetKeyDown("return") && onButton == true)
+        {
+            player.SetActive(true);
+            menu.SetActive(false);
+
+        }
+        //if return is pressed and onButton equals false
+        //then change number of players
+
+        if(Input.GetKeyDown("return") && onButton == false)
+        {
+            if (actualNum.text == "1")
+            {
+                actualNum.text = "2";
+            }
+            else {
+                actualNum.text = "1";
+            }
+        }
+
+        //if position top set onButton to false
+        //if position bottom set onButton to true
+
+        //if (Input.GetKey("return"))
+        //{
+        //    player.SetActive(true);
+        //    menu.SetActive(false);
+        //}
+
+    }
+}
--- ASSIGNMENT 1/Assets/Scripts/playerManager.cs
+++ ASSIGNMENT 1/Assets/Scripts/playerManager.cs
@@ -5,33 +5,52 @@ using UnityEngine;
 public class playerManager : MonoBehaviour {
 
     Rigidbody2D rb2d;
+    Animator anim;
+    SpriteRenderer sprite;
 
 
 	// Use this for initialization
 	void Start () {
         Debug.Log("its working");
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+       
 
-
-        if (Input.GetKey("w"))
+        if (Input.GetKey("w") || Input.GetKey("up"))
         {
             rb2d.velocity = new Vector2(0, 2);
+            anim.Play("PlayerMove");
+            sprite.flipY = false;
         }
-        else if (Input.GetKey("s"))
+        else if (Input.GetKey("s") || Input.GetKey("down"))
         {
             rb2d.velocity = new Vector2(0, -2);
+            anim.Play("PlayerMove");
+            sprite.flipY = true;
         }
-        else if (Input.GetKey("d"))
+        else if (Input.GetKey("d") || Input.GetKey("right"))
         {
             rb2d.velocity = new Vector2(2, 0);
+            anim.Play("PlayerMoveLR");
+            sprite.flipX = false;
         }
-        else if (Input.GetKey("a"))
+        else if (Input.GetKey("a") || Input.GetKey("left"))
         {
             rb2d.velocity = new Vector2(-2, 0);
+            anim.Play("PlayerMoveLR");
+            sprite.flipX = true;
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(0, 0);
+
+            anim.Play("");
+
         }
 
     }
